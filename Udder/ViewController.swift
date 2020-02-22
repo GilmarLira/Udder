aea9fe0b55697189642e24e2b357145b60706a0b
Gilmar Lira <gilmar.lira@me.com>
2020-02-21 17:29:35 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Gilmar Lira on 2/21/20.
+//  Copyright © 2020 Gilmar Lira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
